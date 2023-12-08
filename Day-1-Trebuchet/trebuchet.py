9a8bd101f3e141ce5483fce000c1480e10c23c1b
Thomas Chung <60494228+Thomas-Chung-1997@users.noreply.github.com>
2023-12-07 23:22:16 -0700

    Day 2 Cube Conundrum
    - Added day 2.
    - Moved day 2 Trebuchet part 2 to day 1 (fix).

--- Day-1-Trebuchet/trebuchet.py
+++ Day-1-Trebuchet/trebuchet.py
@@ -1,35 +1,38 @@
+import re
+
+NUMBERS = "(?:zero|one|two|three|four|five|six|seven|eight|nine|[0-9])"
+CONVERT_NUMBERS = {"zero" : 0,"one" : 1, "two" : 2, "three" : 3, "four" : 4, "five" : 5, "six" : 6, "seven" : 7, "eight" : 8, "nine" : 9, "0": 0, "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9}
+
 def main():
     print(Trebuchet())
     
 def Trebuchet():
     count = 0
     
-    with open('test.txt') as file:
+    with open("test.txt") as file:
         for line in file:
             count += CalibrationValue(line)
             
     return count
     
 def CalibrationValue(line):
-    firstDigit = 0
-    secondDigit = 0
-    firstflag = True
+    digits = GetNumbers(line)
+                
+    return digits[0] * 10 + digits[-1]
+
+def GetNumbers(line):
+    digits = re.finditer(NUMBERS, line)
+        
+    digits = list(map(lambda x : x.group(), digits))
     
-    for char in line:
-        if char.isnumeric():
-            number = int(char)
+    return ConvertWordsToNumbers(digits)
+
+def ConvertWordsToNumbers(digits):
+    for i in range(len(digits)):
+        if digits[i] in CONVERT_NUMBERS:
+            digits[i] = CONVERT_NUMBERS[digits[i]]
             
-            if firstflag:
-                firstDigit = number
-                secondDigit = number
-                firstflag = False
-            else:
-                secondDigit = number
-                
-    #print(line)
-    #print(firstDigit * 10 + secondDigit)
-                
-    return firstDigit * 10 + secondDigit
+    return digits
     
 if __name__ == "__main__":
     main()
---
+++ Day-2-Cube-Conundrum/cube.py
@@ -0,0 +1,68 @@
+import re
+
+CONST_RED = 12
+CONST_GREEN = 13
+CONST_BLUE = 14
+CONST_FIND_CUBES = "(\d+ green| \d+ blue| \d+ red)"
+
+def main():
+    print(Cube())
+    
+def Cube():
+    count = 0
+    
+    with open("test.txt") as file:
+        # PART 1
+        #i = 1
+        #for line in file:
+            
+        #    if Game(line):
+        #        count += i
+            
+        #    i += 1
+        
+        for line in file:
+            count += Game(line)
+            
+    return count
+        
+def Game(line):
+    red = 0
+    green = 0
+    blue = 0
+    cubes = GetCubes(line)
+    
+    #print(line)
+    
+    for c in cubes:
+        c = c.split()
+        c[0] = int(c[0])
+        
+        #print(c[0])
+        
+        if c[1] == "red" and c[0] > red:
+            red = c[0]
+        elif c[1] == "green" and c[0] > green:
+            green = c[0]
+        elif c[1] == "blue" and c[0] > blue:
+            blue = c[0]
+            
+        #print(str(red) + ' ' + str(green) + ' ' + str(blue))
+    
+    # PART 1
+    #if red <= CONST_RED and green <= CONST_GREEN and blue <= CONST_BLUE:
+    #    return True
+    #else:
+    #    return False  
+    
+    return red * green * blue
+
+def GetCubes(line):
+    cubes = re.finditer(CONST_FIND_CUBES, line)
+        
+    return list(map(lambda x : x.group(), cubes))
+    
+
+        
+if __name__ == "__main__":
+    main()
